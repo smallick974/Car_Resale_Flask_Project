f8b2f82c87e0e151a9526885a49237e50624bcd2
Srijan Mallick <smallick974@gmail.com>
2023-04-29 23:42:34 +0530

    Update init.py

--- Car_Resale_Flask/car_resale/__init__.py
+++ Car_Resale_Flask/car_resale/__init__.py
@@ -10,7 +10,6 @@ parser.read('prjconfigs.cfg')
 # to get the value of secret key and convert the returned values from list to string using ''.join()
 secret_key = ''.join([parser['SECRET_KEY'][key] for key in parser['SECRET_KEY'] if 'SECRET_KEY' in parser])
 
-carapp = Flask(__name__)
 carapp.config['SECRET_KEY'] = secret_key
 
 from car_resale import routes
